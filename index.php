19da564d187510fb77070df81d36e71a7f0cce82
justtice-ankomah <47444006+justtice-ankomah@users.noreply.github.com>
2019-07-12 03:34:28 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Codenow.com</title>
+    <link rel="stylesheet" href="assets/css/main.css" />
+    <link rel="stylesheet" href="asset/css/indexcss.css">
+    <link href="asset/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="asset/css/animate.css">
+</head>
+<body title="CONDENOW.COM">
+    <!-- Header -->
+    <header id="header" class="alt">
+      <div class="logo"><a href="index.php"><img  src="assets/images/logo.png"></a></div>
+      <a id="rightmenu" href="#menu">Topics</a>
+      </header>
+<!-- Nav -->
+    <nav id="menu">
+      <ul class="links">
+        <li><a href="#">Web Development</a>
+        <p><a class="programes" href="#">html</a></p>
+        <p><a class="programes" href="#">css</a></p>
+        <p><a class="programes" href="#">javascript</a></p>
+        <p><a class="programes" href="#">php</a></p>
+        <p><a class="programes" href="#">mysl</a></p>
+        <p><a class="programes"  href="">ajax</a></p>
+        </li>
+        <li><a href="#">Mobile Apps</a>
+          <p><a class="programes" href="#">jave</a></p>
+          <p><a class="programes" href="">react native</a></p>
+          <p><a class="programes" href="#">kotlin</a></p>
+          <p><a class="programes" href="">swif</a></p>
+        </li>
+        <li><a  href="#">Windows Apps</a>
+          <p><a class="programes" href="#">c++</a></p>
+          <p><a class="programes" href="">c#</a></p>
+          <p><a class="programes" href="#">c</a></p>
+        </li>
+      </ul>
+    </nav>
+    <!--nav-->
+<br/>
+    <div id="navigation">
+<a href="#" class="navlist" id="home">Home </a>
+<a href="#" class="navlist">About </a>
+<a href="#" class="navlist">Contact</a>
+<div id="reg"> <a href="Register.php" id="Register">Register</a><a href="login.php" id="login">Login</a></div>
+    </div>
+<!--resouces-->
+<div id="resouces">
+  <div>
+<p style="color:white;">Welcome To The Best Supplier Of resouces For Tutorials Videos </p>
+<center>
+<p style="color:white;"><h3 style="color:white;">FOR FREE</h3></P>
+<p style="color:red;text-decoration:underline;"><a href="#" id="let">Let's Get You start </a></p>
+</center>
+</div>
+
+<div>
+  <br/>
+  <br/>
+<img id="vid" src="assets/images/hii.png">
+
+</div>
+</div>
+<!--search-->
+<div id="search">
+  <div id="one">
+<input id="on" type="text" name="" value="search">
+</div>
+<div id="two">
+  populler tutorial videos links
+</div>
+</div>
+
+  <!--links-->
+<div id="links">
+<div id="flink">
+  <a class="udemy" href="#">Udemy </a>
+</div>
+<div id="slink">
+      <a class="udemy" href="#">Udemy </a>
+</div>
+<div id="tlink">
+  <a class="udemy" href="#">Udemy </a>
+</div>
+<div id="folink">
+    <a class="udemy" href="#">Udemy </a>
+</div>
+</div>
+<!--view all button-->
+<center>
+<button id="viewall"> View All</button>
+</center>
+<!--latest videos = the img will be videos-->
+<br/>
+<br/>
+<div id="con">
+  <center>
+<h4 style="color:red;"> Latext New Videos</h4>
+
+<div id="latest">
+<div class="v"><img class="p" src="assets/images/video.png"></div>
+<div class="v"><img class="p" src="assets/images/video.png"></div>
+<div class="v"><img class="p" src="assets/images/video.png"></div>
+</div>
+  </center>
+</div>
+<!--topics follows -->
+<br/>
+<br/>
+<div id="all_container">
+<center>
+  <h4 style="color:red;">topics</h4>
+</center>
+<div id="tops_container">
+  <div class="tops">
+<p style="color:red;">Web Development</p>
+<ul>
+  <li><a href="#">html</a></li>
+    <li><a href="#">css</a></li>
+      <li><a href="#">javascript</a></li>
+      <li><a href="#">php</a></li>
+      <li><a href="#">mysql</a></li>
+        <li><a href="#">ajax</a></li>
+</u>
+</div>
+<div class="tops">
+  <p style="color:red;">Mobile Apps</p>
+  <ul>
+    <li><a href="#">java</a></li>
+      <li><a href="#">kotlin</a></li>
+        <li><a href="#">react native</a></li>
+        <li><a href="#">swif</a></li>
+  </u>
+</div>
+<div class="tops">
+  <p style="color:red;">Windows Apps</p>
+  <ul>
+    <li><a href="#">c++</a></li>
+      <li><a href="#">c#</a></li>
+        <li><a href="#">c</a></li>
+  </u>
+</div>
+</div>
+</div>
+<!--request links follows-->
+<br/>
+<br/>
+<div>
+  <center>
+<p style="color:red;">you can click on the bellow button to request for tutorial video</p>
+<div id="pop">
+<form action="" method="post">
+  <button style="color:white;float:right;position:relative;right:10px;" onclick="document.getElementById('pop').style.display='none'">x</button>
+  <input class="in" name="user_name" placeholder="User Name">
+  <br/>
+  <input class="in" name="email" placeholder="Email">
+  <br/>
+  <textarea class="in">enter request description</textarea>
+  <br/>
+  <input id="request" type="submit" value="Request">
+</form>
+</div>
+<button id="req" onclick="document.getElementById('pop').style.display='block'">Request</button>
+</center>
+</div>
+  <!-- Scripts -->
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/jquery.scrolly.min.js"></script>
+    <script src="assets/js/jquery.scrollex.min.js"></script>
+    <script src="assets/js/skel.min.js"></script>
+    <script src="assets/js/util.js"></script>
+    <script src="assets/js/main.js"></script>
+</body>
+</html>
